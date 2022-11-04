de1d72373a5b9acbc28e4405b8031562f5779bdd
Dima Pulkinen <dima@itsemast.com>
2022-11-04 07:21:13 +0000

    fix automatic middleware loading

--- server/middlewares/logger.ts
+++ server/middlewares/logger.ts
@@ -5,24 +5,22 @@ import { Highlighter } from '../highlighter';
  * Based on this post: https://www.reddit.com/r/Strapi/comments/fzg4j6/comment/fn9eulv/
  */
 
-export default () => {
-  return async (ctx, next) => {
-    // only log graphql post requests
-    if (ctx.request.url === '/graphql' && ctx.request.method === 'POST') {
-      await next();
+export default async (ctx, next) => {
+  // only log graphql post requests
+  if (ctx.request.url === '/graphql' && ctx.request.method === 'POST') {
+    await next();
 
-      const query = ctx.request.body?.query;
+    const query = ctx.request.body?.query;
 
-      // don't print huge introspection queries
-      if (query.startsWith("query IntrospectionQuery ")) {
-        console.log("Introspection query ignored");
-      } else {
-        // print highlighted query and response
-        console.log(Highlighter.highlight(query, "graphql"));
-        console.log(Highlighter.highlight(ctx.body, "json"));
-      }
+    // don't print huge introspection queries
+    if (query.startsWith("query IntrospectionQuery ")) {
+      console.log("Introspection query ignored");
     } else {
-      await next();
+      // print highlighted query and response
+      console.log(Highlighter.highlight(query, "graphql"));
+      console.log(Highlighter.highlight(ctx.body, "json"));
     }
-  };
+  } else {
+    await next();
+  }
 };
